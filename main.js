6251fbca0dc7b7174803b3f8965bd907680f4196
KenzoRom <302808825@student.rocmondriaan.nl>
2021-06-29 01:28:58 +0200

    Complete

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-const themeButton = document.querySelector('.switch');
+const enableTheme = document.querySelector('.theme-enable');
+const disableTheme = document.querySelector('.theme-disable');
+let currentMode = localStorage.getItem("darkmode");
+let elements = document.querySelectorAll('.header, section, .selfie-img');
+let theme = currentMode;
 
-let darkMode = 0;
-
-const swapTheme = function(){
-    const elements = document.querySelectorAll('.header, section, .selfie-img');
-        for (let i = 0; i < elements.length; i++){
-            elements[i].classList.add('dark');
-            console.log('jmor')
-        }
-
-    
+if(theme == 'dark'){
+    for (let i = 0; i < elements.length; i++){
+        elements[i].classList.add('dark');
+    }
 }
 
-themeButton.addEventListener('click', swapTheme)
+enableTheme.addEventListener('click', () => {
+    for (let i = 0; i < elements.length; i++){
+            elements[i].classList.add('dark');
+        }
+        theme = 'dark';
+        localStorage.setItem("darkmode", theme);
+})
 
+disableTheme.addEventListener('click', () => {
+    for (let i = 0; i < elements.length; i++){
+        elements[i].classList.remove('dark');
+    }
+    theme = 'light';
+    localStorage.setItem("darkmode", theme);
+})
